6cdf3290408f74366865e1d33aae3a43ac26cd66
Skakit Akilla <86703988+Swag369@users.noreply.github.com>
2025-10-26 14:24:19 -0400

    LC-min-stack [Runtime: 12 ms; Memory: 21.4 MB]

--- design/min-stack.py
+++ design/min-stack.py
@@ -1,27 +1,25 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
-        self.min = []
-        
+        self.arr = []
+        self.min = []                
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if len(self.min) == 0 or val < self.min[-1]:
+        if not self.arr:
             self.min.append(val)
         else:
-            self.min.append(self.min[-1])
+            self.min.append(min(self.min[-1], val))
+        self.arr.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        self.arr.pop()
         self.min.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.arr[-1]
 
     def getMin(self) -> int:
         return self.min[-1]
-        
 
 
 # Your MinStack object will be instantiated and called as such:
