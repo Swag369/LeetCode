8280ba2868d50202dfbf480d539d6fff6a1e36f3
Skakit Akilla <86703988+Swag369@users.noreply.github.com>
2025-10-26 16:03:05 -0400

    LC-3sum [Runtime: 2508 ms; Memory: 21.5 MB]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -1,29 +1,43 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = []
+        seen_i = set()
+        smallest_key_triplet_map = dict()
 
-        oldi = nums[0]-1
+        # print(nums)
 
         for i in range(len(nums)):
-        
-            if nums[i] == oldi:
-                continue
-            else:
-                oldi = nums[i]
-        
-            j = i+1
-            k = len(nums)-1
 
-            while (j < k):
-                if nums[j] + nums[k] + nums[i] > 0:
-                    k -= 1
-                elif nums[j] + nums[k] + nums[i] < 0:
+            if i in seen_i:
+                continue
+            
+            mini_total = 0 - nums[i]
+
+            k = len(nums) - 1
+            j = i + 1
+
+            while(j < k):
+                # print("i,j,k", i, j, k)
+                if nums[j] + nums[k] < mini_total:
                     j += 1
+                elif nums[j] + nums[k] > mini_total:
+                    k -= 1
                 else:
-                    ans.append([nums[i], nums[j], nums[k]])
-                    oldj = nums[j]
-                    while j < k and nums[j] == oldj:
-                        j += 1
-                    
-        return ans
+                    # print("worked!")
+                    if nums[i] in smallest_key_triplet_map:
+                        smallest_key_triplet_map[nums[i]].add((nums[i], nums[j], nums[k]))
+                    else:
+                        smallest_key_triplet_map[nums[i]] = {(nums[i], nums[j], nums[k])}
+                        # smallest_key_triplet_map[i].add((nums[i], nums[j], nums[k]))
+                    # print(smallest_key_triplet_map)
+                    j += 1
+
+
+        # print(smallest_key_triplet_map)
+
+        ret = []
+        for i in smallest_key_triplet_map.keys():
+            for j in smallest_key_triplet_map[i]:
+                ret.append(j)
+
+        return ret
