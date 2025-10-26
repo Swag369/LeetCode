17f97ce971ec6e6fc0644c68fa2a995c087f4a44
Skakit Akilla <86703988+Swag369@users.noreply.github.com>
2025-10-26 15:32:44 -0400

    LC-climbing-stairs-ii [Runtime: N/A; Memory: N/A]

--- array/climbing-stairs-ii.py
+++ array/climbing-stairs-ii.py
@@ -12,14 +12,19 @@ class Solution:
 
         costs.insert(0,0)
 
-        dp = [0 for _ in costs]
+        dp = [1 for _ in costs]
 
         dp[n] = 0
-        dp[n-1] = cost(n, n-1)
-        dp[n-2] = min(cost(n, n-2), dp[n-1] + cost(n-1, n-2))
-        dp[n-3] = min(cost(n, n-3), dp[n-1] + cost(n-1, n-3), dp[n-2] + cost(n-2, n-3))
+        if n >= 1:
+            dp[n-1] = cost(n, n-1)
+        if n >= 2:
+            dp[n-2] = min(cost(n, n-2), dp[n-1] + cost(n-1, n-2))
+        if n >= 3:
+            dp[n-3] = min(cost(n, n-3), dp[n-1] + cost(n-1, n-3), dp[n-2] + cost(n-2, n-3))
 
         for i in range(n-4, -1, -1):
             dp[i] = min(dp[i+3] + cost(i+3, i), dp[i+2] + cost(i+2, i), dp[i+1] + cost(i+1, i))
         
+        print(costs)
+        print(dp)
         return dp[0]
